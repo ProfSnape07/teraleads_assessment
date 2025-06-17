3e89267331cd8db03df91af73c82b98f856e769c
Prof Snape <95466549+ProfSnape07@users.noreply.github.com>
2025-06-18 02:31:56 +0400

    update CORS allow_origins policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # allow all origins
+    allow_origins=["http://localhost:3000"],  # allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
